033ee037193e4c4a7fced648e1d5282347b68ba2
Vinod Sridharan <visridha@microsoft.com>
2020-07-23 03:57:15 -0700

    More Perf improvements
    # Conflicts:
    #	Microsoft.Azure.Cosmos/src/StringHMACSHA256Hash.cs

--- Microsoft.Azure.Cosmos/src/StringHMACSHA256Hash.cs
+++ Microsoft.Azure.Cosmos/src/StringHMACSHA256Hash.cs
@@ -5,6 +5,7 @@
 namespace Microsoft.Azure.Cosmos
 {
     using System;
+    using System.Collections.Concurrent;
     using System.Security;
     using System.Security.Cryptography;
 
@@ -13,18 +14,33 @@ namespace Microsoft.Azure.Cosmos
         private readonly String base64EncodedKey;
         private readonly byte[] keyBytes;
         private SecureString secureString;
+        private ConcurrentQueue<HMACSHA256> hmacPool;
 
         public StringHMACSHA256Hash(String base64EncodedKey)
         {
             this.base64EncodedKey = base64EncodedKey;
             this.keyBytes = Convert.FromBase64String(base64EncodedKey);
+            this.hmacPool = new ConcurrentQueue<HMACSHA256>();
         }
 
         public byte[] ComputeHash(byte[] bytesToHash)
         {
-            using (HMACSHA256 hmacSha256 = new HMACSHA256(this.keyBytes))
+            if (this.hmacPool.TryDequeue(out HMACSHA256 hmacSha256))
+            {
+                hmacSha256.Initialize();
+            }
+            else
+            {
+                hmacSha256 = new HMACSHA256(this.keyBytes);
+            }
+
+            try
             {
                 return hmacSha256.ComputeHash(bytesToHash);
+            }
+            finally
+            {
+                this.hmacPool.Enqueue(hmacSha256);
             }
         }
 
@@ -40,6 +56,11 @@ namespace Microsoft.Azure.Cosmos
 
         public void Dispose()
         {
+            while (this.hmacPool.TryDequeue(out HMACSHA256 hmacsha256))
+            {
+                hmacsha256.Dispose();
+            }
+
             if (this.secureString != null)
             {
                 this.secureString.Dispose();
